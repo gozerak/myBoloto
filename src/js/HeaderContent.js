7ecdd183eb9021eaab5c9ff74d8e526abe44a9ed
Санатов Ильдар Русланович <SanatovIR@MILKOM-KOMOS.RU>
2024-07-26 16:23:38 +0400

    Профиль и выход для профиля

--- src/js/HeaderContent.js
+++ src/js/HeaderContent.js
@@ -4,58 +4,56 @@ import { NavLink } from 'react-router-dom';
 import HeaderLogin from "./HeaderLogin";
 import ProfileIcon from "./ProfileIcon";
 
-function HeaderName () {
+function HeaderName() {
     return (
         <p className="header-name">KOMOS JOBHUB</p>
-    )
+    );
 }
 
 function HeaderLogo() {
     return (
         <img src='/img/pic_logo.svg' alt='logo' />
-    )
+    );
 }
 
 export function HeaderChapters() {
-    return(
+    return (
         <>
-        <NavLink to="/" className={({ isActive }) => isActive ? "chapter-executor active-link" : "chapter-executor"} >
-        <div id="for-executor">Поиск работы</div>
-        </NavLink>
-        <NavLink to="/customer" className={({ isActive }) => isActive ? "chapter-customer active-link" : "chapter-customer"}>
-        <div id="for-customer">Мои заказы</div>
-        </NavLink>
+            <NavLink to="/" className={({ isActive }) => isActive ? "chapter-executor active-link" : "chapter-executor"}>
+                <div id="for-executor">Поиск работы</div>
+            </NavLink>
+            <NavLink to="/customer" className={({ isActive }) => isActive ? "chapter-customer active-link" : "chapter-customer"}>
+                <div id="for-customer">Мои заказы</div>
+            </NavLink>
         </>
-    )
+    );
 }
 
 function ReportProblem() {
-    return(
+    return (
         <p className="report-problem">Сообщить о проблеме</p>
-    )
+    );
 }
-
 
 export default function HeaderContent() {
     let accessToken = null;
 
     if (localStorage.getItem('userId')) {
-      const cookieString = document.cookie;
-      const cookies = cookieString.split('; ').find(row => row.startsWith('accessToken'));
-    
-      if (cookies) {
-        accessToken = cookies.split('=')[1];
-      }
-    } 
+        const cookieString = document.cookie;
+        const cookies = cookieString.split('; ').find(row => row.startsWith('accessToken'));
+
+        if (cookies) {
+            accessToken = cookies.split('=')[1];
+        }
+    }
 
     return (
         <>
-        <HeaderLogo />
-        <HeaderName />
-        <HeaderChapters />
-        {accessToken? <ProfileIcon /> : <HeaderLogin />}
-        <ReportProblem />
-        </>   
-    )
-    
-};
+            <HeaderLogo />
+            <HeaderName />
+            <HeaderChapters />
+            {accessToken ? <ProfileIcon /> : <HeaderLogin />}
+            <ReportProblem />
+        </>
+    );
+}
--- src/js/ProfileIcon.js
+++ src/js/ProfileIcon.js
@@ -1,10 +1,34 @@
-import { UserContext } from "./UserContext"
-import { useContext } from "react"
-import "../css/ProfileIcon.css"
+import { useContext, useState } from "react";
+import { UserContext } from "./UserContext";
+import "../css/ProfileIcon.css";
 
-export default function ProfileIcon () {
-    const { userData } = useContext(UserContext);
+export default function ProfileIcon() {
+    const { userData, setUserData } = useContext(UserContext);
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setDropdownOpen(!isDropdownOpen);
+    };
+
+    const handleLogout = () => {
+        // Логика выхода из системы
+        localStorage.removeItem('userId');
+        document.cookie = 'accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        setUserData(null);
+        window.location.reload();
+    };
+
     return (
-        <div className="user-icon">{userData? userData.login : "Loading..."}</div>
-    )
+        <div className="user-icon-container">
+            <div className="user-icon" onClick={toggleDropdown}>
+                {userData ? userData.login : "Loading..."}
+            </div>
+            {isDropdownOpen && (
+                <div className="dropdown-menu">
+                    <button className="dropdown-item">Профиль</button>
+                    <button className="dropdown-item" onClick={handleLogout}>Выйти</button>
+                </div>
+            )}
+        </div>
+    );
 }
